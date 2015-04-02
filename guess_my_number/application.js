3d0c0143da4a14141e83e752216cfdb4f86159b0
moryk <mkeita@kippnyccp.org>
2015-04-02 13:30:47 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,35 +1,28 @@
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  var randomnumber=Math.ceil(Math.random()*100)
-//   var arr = []
-// while(arr.length < 1){
-//   var randomnumber=Math.ceil(Math.random()*100)
-//   var found=false;
-//   for(var i=0;i<arr.length;i++){
-// 	if(arr[i]==randomnumber){found=true;break}
-//   }
-//   if(!found)arr[arr.length]=randomnumber;
-// }
-// document.write(arr);
-
-  
+  var chooseNumber = Math.floor(Math.random()*100);
   //when the player clicks on the 'guess' button
   $('button').on('click', function(){
     //save their guess as a variable
-    
-    
+    var guess = parseInt($('input').val());
+    $('#feedback').fadeOut().empty().fadeIn();
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
-    
+    if(guess === chooseNumber){
       //what happens if the guess is correct?
-      
+      $('#feedback').append('You are correct!');
+    }
     //if the user guessed too high...
-    
+    if(guess > chooseNumber){
       //update the 'feedback' paragraph to tell them to guess lower
-      
+      $('#feedback').append('Guess lower!');
+    }
     //otherwise, the user guessed too low...
-    
+    if(guess < chooseNumber){
       //update the 'feedback' paragraph to tell them to guess higher
-    
-  });
+      $('#feedback').append('Go higher!');
+    }
+      $('input').val(""));
+  })
 });
+
