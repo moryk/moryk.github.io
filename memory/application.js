03042a8e02f8628ea46b41d3b1a7dd2bd24eeb96
moryk <mkeita@kippnyccp.org>
2015-04-13 13:33:51 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]]
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
   //these are the names that will be on the card
   function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
