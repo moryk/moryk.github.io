36f3515accc6e998e2b0e600041765574b83c71b
moryk <mkeita@kippnyccp.org>
2015-04-16 13:23:18 -0400

    Update application.js

--- shopping_list/application.js
+++ shopping_list/application.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $('button').on('click',function(){
       var input=$('input').val();
        $('#item-holder').append("<p>"+input+"</p>") // help to save #
+       <input type="button" value="Reset Form" onClick="this.form.reset()" />
   });
 });
