62e85a36275a485c7a7bdebbbaf4a606e8a96039
moryk <mkeita@kippnyccp.org>
2015-04-16 13:31:47 -0400

    Update application.js

--- shopping_list/application.js
+++ shopping_list/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  $('button').on('click',function(){
+  $('#submit').on('click',function(){
       var input=$('input').val();
        $('#item-holder').append("<p>"+input+"</p>") // help to save #
+  });
+  $('#clear').on('click',function(){
+  .empty().append(guesses +1)
   });
 });
