f8e6fb2d9b10bd94909f201a0ff011c23c5a57e7
moryk <mkeita@kippnyccp.org>
2015-05-12 13:29:38 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,15 +1,15 @@
 from random import randint
-total=0 # sets the total elections won 
-for i in range(10000): # repeats procces 10000 times because the range is 10,000
-  wins=0  #sets wins in regions to 0 
-  if randint(1,100) <= 87: #picks a number between 1-100 and if its between 1-87 - follow next step 
-    wins=wins+1 # add smaller regional wins to wins 
-  if randint(1,100) <= 65:  #picks a number between 1-100 and if its between 1-64 - follow next step 
-    wins=wins+1# add smaller regional wins to wins 
-  if randint(1,100) <= 17: #picks a number between 1-100 and if its between 1-17 - follow next step 
-    wins=wins+1 # add smaller regional wins to wins 
-  if wins>=2: #dettermined if has 2 or more regional wins
-    total+=1 # if so thean adds to toal aka the complete election 
+total=0 # the number that you start with is 0
+for i in range(10000): # do everything below over ten thousand times
+  wins=0  #sets wins to 0 
+  if randint(1,100) <= 87: #picks a number between 1 and 100. if the number is between 1 and 87 then add 1 to the number of wins
+    wins=wins+1 # then add 1 to number of wins
+  if randint(1,100) <= 65:  #picks a number between 1 and 100. if the number is between 1 and 67 then add 1 to the number of wins
+    wins=wins+1# then add 1 to number of wins
+  if randint(1,100) <= 17: #picks a number between 1 and 100. if the number is between 1 and 17 then add 1 to the number of wins
+    wins=wins+1 # then add 1 to number of wins
+  if wins>=2: #compare the number of wins to see if its greater 2 
+    total+=1 # then add 1 to total 
 chance= (total/((float(10000))) # makes it into a percent to print out 
-print "your chance in winning is %s" % chance #prints out statement and percent 
+print "your chance in winning is %s" % chance #prints out the statement in quotes
   
